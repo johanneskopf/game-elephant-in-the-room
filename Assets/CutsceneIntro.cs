33a457e6ba441d555a8e6ab4ad365ab86255031d
johanneskopf <johanneskopf@gmx.at>
2024-01-27 18:38:57 +0100

    cutscene - elephant animation

--- Assets/CutsceneIntro.cs
+++ Assets/CutsceneIntro.cs
@@ -5,6 +5,7 @@ public class CutsceneIntro : MonoBehaviour
 {
     public GameObject elephant1;
     public GameObject elephant2;
+    public GameObject elephant2Placeholder;
 
     void Start()
     {
@@ -13,12 +14,16 @@ public class CutsceneIntro : MonoBehaviour
 
     void Part1()
     {
-        var elephant2RendererGO = elephant2.transform.GetChild(0).gameObject;
-        SetGOActive(false, elephant2RendererGO);
+        //var elephant2RendererGO = elephant2.transform.GetChild(0).gameObject;
+        //SetGOActive(false, elephant2RendererGO);
         Sequence mySequence = DOTween.Sequence();
         mySequence.Append(elephant1.transform.DOMoveX(15, 3))
-          .Join(elephant2.transform.DOMoveX(30, 3))
-          .PrependInterval(1).AppendCallback(() => SetGOActive(true, elephant2RendererGO)).Append(elephant2.transform.DOScaleX(-1, 0)).Append(elephant2.transform.DOMoveX(19, 3));
+          .Join(elephant2Placeholder.transform.DOMoveX(30, 3)).AppendInterval(0.5f)
+          //.PrependInterval(1)
+          /*AppendCallback(() => SetGOActive(true, elephant2RendererGO)).*/
+          /*.Append(elephant2.transform.DOScaleX(-1, 0))*/
+          .Append(elephant2.transform.DOMoveX(19, 3))
+          .Append(elephant2Placeholder.transform.DOMoveX(19, 3));
         DOTween.PlayAll();
     }
 
