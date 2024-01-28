a40875d8a461f57d40d6de26b9dcb8fcb68f3a31
johanneskopf <johanneskopf@gmx.at>
2024-01-28 10:56:48 +0100

    cutscene - finish intro

--- Assets/Scripts/CutsceneIntro.cs
+++ Assets/Scripts/CutsceneIntro.cs
@@ -1,10 +1,12 @@
 using DG.Tweening;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CutsceneIntro : MonoBehaviour
 {
     public GameObject elephant1;
+    public GameObject elephant1Placeholder;
     public GameObject elephant2;
     public GameObject elephant2Placeholder;
     public TMPro.TextMeshProUGUI dialogText;
@@ -12,19 +14,37 @@ public class CutsceneIntro : MonoBehaviour
     public GameObject person1;
 
     private bool WaitsForAccept = false;
-    private int MaxDialogLines = 0;
+    private int CurrentDialogLines = 0;
     private int CurrentDialogLine = 0;
 
     List<string> dialogLines = new List<string> {
         "Russel: Hey Dussel, let's play a quick round truth or dare.",
+        "Dussel: Okay! Truth or dare?",
+        "Russel: Dare.",
         "Dussel trumpets, then silence, then an idea!",
-        "Dussel: Yes, let's always say YES for a day to everyone."
+        "Dussel: I dare you: Always say YES to everything for one day.",
+        "Russel: Sounds like a very positive thing to do, I like that!",
+        "Russel: Now it's my turn. Truth or dare?",
+        "Dussel: Dare.",
+        "Russel: I dare you: Always say YES to everything for one day, one second.",
+        "Dussel: Hmm.. sounds familiar.",
+    };
+
+    List<string> dialogLines2 = new List<string> {
+        "Random Person: I am so thirsty!",
+        "Random Person: You there, elephants, can you give me some water?",
+        "Russel and Dussel trumpet.",
+        "Russel + Dussel: YES!"
+    };
+
+    List<string> dialogLines3 = new List<string> {
+        "Russel: In this skyscraper, we find a glas of water, don't we?",
+        "Dussel: YES!",
+        "Russel and Dussel go in..",
     };
 
     void Start()
     {
-        MaxDialogLines = dialogLines.Count;
-
         SetCanvasActive(false, dialogCanvasGroup);
 
         Part1();
@@ -37,6 +57,31 @@ public class CutsceneIntro : MonoBehaviour
         dialogCanvasGroup.blocksRaycasts = active;
     }
 
+    List<string> GetCurrentDialogLines()
+    {
+        if(CurrentDialogLines == 0)
+        {
+            return dialogLines;
+        }
+        else if (CurrentDialogLines == 1)
+        {
+            return dialogLines2;
+        }
+        else if(CurrentDialogLines == 2)
+        {
+            return dialogLines3;
+        }
+        return new List<string>();
+    }
+
+    string GetCurrentDialogLine()
+    {
+        var dialogLines = GetCurrentDialogLines();
+        if (CurrentDialogLine >= dialogLines.Count)
+           return "";
+        return dialogLines[CurrentDialogLine];
+    }
+
     void Part1()
     {
         //var elephant2RendererGO = elephant2.transform.GetChild(0).gameObject;
@@ -45,38 +90,81 @@ public class CutsceneIntro : MonoBehaviour
 
         // append elephant meet
         rootSequence.Append(elephant1.transform.DOMoveX(7.35f, 3))
+          .Join(elephant1Placeholder.transform.DOMoveX(7.35f, 3))
           .Join(elephant2Placeholder.transform.DOMoveX(12.0f, 5))
           //.AppendInterval(0.5f)
           .AppendInterval(-4.0f)
           .Join(elephant2.transform.DOMoveX(8.6f, 3))
           .Join(elephant2Placeholder.transform.DOMoveX(8.6f, 4.5f));
 
-        //AppendDialogLineToSequence(dialogLines[CurrentDialogLine], rootSequence, dialogText);
-
-        // append person meet
-        rootSequence.Append(person1.transform.DOMoveX(8.0f, 2))
-        .Append(person1.transform.DOMoveZ(0.465f, 2))
-        .Join(person1.transform.DORotate(new Vector3(0f, 180f, 0f), 0.5f));
+        // append dialog
+        AppendDialogLineToSequence(GetCurrentDialogLine(), GetCurrentDialogLines(), rootSequence, dialogText);
 
         rootSequence.Play();
         //DOTween.PlayAll();
     }
 
+    void Part2()
+    {
+        Sequence sequencePersonMeet = DOTween.Sequence();
+        // append person meet
+        sequencePersonMeet.Append(person1.transform.DOMoveX(8.0f, 2))
+        .Append(person1.transform.DOMoveZ(0.465f, 2))
+        .Join(person1.transform.DORotate(new Vector3(0f, 180f, 0f), 0.5f));
+
+        // append dialog
+        AppendDialogLineToSequence(GetCurrentDialogLine(), GetCurrentDialogLines(), sequencePersonMeet, dialogText);
+
+        sequencePersonMeet.Play();
+    }
+
+    void Part3()
+    {
+        Sequence sequenceElephantsGoToSkyscraper = DOTween.Sequence();
+        sequenceElephantsGoToSkyscraper.Append(elephant1.transform.DOMoveX(3.33f, 2))
+        .Join(elephant1Placeholder.transform.DOMoveX(3.33f, 2))
+        .Join(elephant2Placeholder.transform.DOMoveX(4.33f, 5))
+        .Join(elephant2.transform.DOMoveX(4.33f, 5))
+        .Append(elephant1Placeholder.transform.DOMoveY(10.0f, 3))
+        .Join(elephant2Placeholder.transform.DOMoveY(10.0f, 3))
+        .AppendInterval(2)
+        .Append(elephant1Placeholder.transform.DOMoveY(0.0f, 3))
+        .Join(elephant2Placeholder.transform.DOMoveY(0.0f, 3));
+
+        // append dialog
+        AppendDialogLineToSequence(GetCurrentDialogLine(), GetCurrentDialogLines(), sequenceElephantsGoToSkyscraper, dialogText);
+
+        sequenceElephantsGoToSkyscraper.Play();
+    }
+
+    //void Part4()
+    //{
+    //    Sequence sequenceSkyscraperShowOff = DOTween.Sequence();
+    //    sequenceSkyscraperShowOff.Append(elephant1Placeholder.transform.DOMoveY(10.0f, 3))
+    //    .Join(elephant2Placeholder.transform.DOMoveY(10.0f, 3));
+
+    //    sequenceSkyscraperShowOff.Play();
+    //}
+
     public void GoToNextDialog()
     {
         if (!WaitsForAccept)
+        {
             return;
+        }
+
+        Debug.Log("Input - Go To Next Dialog Line");
 
         WaitsForAccept = false;
 
         Sequence sequenceElephantMeet = DOTween.Sequence();
-        AppendDialogLineToSequence(dialogLines[CurrentDialogLine], sequenceElephantMeet, dialogText);
-
+        AppendDialogLineToSequence(GetCurrentDialogLine(), GetCurrentDialogLines(), sequenceElephantMeet, dialogText);
         sequenceElephantMeet.Play();
     }
 
-    void AppendDialogLineToSequence(string dialogLine, Sequence root, TMPro.TextMeshProUGUI dialogText)
+    void AppendDialogLineToSequence(string dialogLine, List<string> dialogLines, Sequence root, TMPro.TextMeshProUGUI dialogText)
     {
+        int MaxDialogLines = dialogLines.Count;
         Sequence sequenceDialog = DOTween.Sequence();
         sequenceDialog.AppendCallback(() =>
         {
@@ -84,19 +172,67 @@ public class CutsceneIntro : MonoBehaviour
             dialogText.maxVisibleCharacters = 0;
             SetCanvasActive(true, dialogCanvasGroup);
         });
-        for (int i = 0; i < dialogLine.Length; i++)
+        for (int i = 0; i <= dialogLine.Length; i++)
         {
             int localI = i;
             sequenceDialog.AppendCallback(() =>
             {
                 IncreaseMaxVisibleChars(dialogText, localI);
-            }).AppendInterval(0.05f);
+            }).AppendInterval(0.03f);
         }
         sequenceDialog.AppendCallback(() => 
         {
-            if (CurrentDialogLine >= MaxDialogLines - 1)
+            if (CurrentDialogLine >= MaxDialogLines)
+            {
+                if(CurrentDialogLines == 0)
+                {
+                    Debug.Log("Finished part 1");
+
+                    // go to next dialog
+                    CurrentDialogLines++;
+                    CurrentDialogLine = 0;
+                    Debug.Log($"Next Dialog line: {dialogLine}, CurrentDialogLine: {CurrentDialogLine}, CurrentDialogLines: {CurrentDialogLines}");
+
+                    // start playing next part
+                    Part2();
+                }
+                else if (CurrentDialogLines == 1)
+                {
+                    Debug.Log("Finished part 2");
+
+                    // go to next dialog
+                    CurrentDialogLines++;
+                    CurrentDialogLine = 0;
+                    Debug.Log($"Next Dialog line: {dialogLine}, CurrentDialogLine: {CurrentDialogLine}, CurrentDialogLines: {CurrentDialogLines}");
+
+                    // start playing next part
+                    Part3();
+                }
+                //else if (CurrentDialogLines == 2)
+                //{
+                //    Debug.Log("Finished part 3");
+
+                //    //// go to next dialog
+                //    //CurrentDialogLines++;
+                //    //CurrentDialogLine = 0;
+                //    //Debug.Log($"Next Dialog line: {dialogLine}, CurrentDialogLine: {CurrentDialogLine}, CurrentDialogLines: {CurrentDialogLines}");
+
+                //    // start playing next part
+                //    Part4();
+                //}
+                else if (CurrentDialogLines >= 2)
+                { // finished intro, start playing!
+
+                    // TODO insert level start!
+                    Debug.Log("Finished all parts");
+                }
+                SetCanvasActive(false, dialogCanvasGroup);
                 return;
+            }
+
             CurrentDialogLine++;
+            Debug.Log($"Next Dialog line: {dialogLine}, CurrentDialogLine: {CurrentDialogLine}, CurrentDialogLines: {CurrentDialogLines}");
+
             WaitsForAccept = true;
         });
         root.Append(sequenceDialog);
